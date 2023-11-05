05a1e9beef67ca735297ba1a0ac39a4bfbede36c
Loren Lei <22lorenlei@gmail.com>
2023-11-04 20:54:31 -0400

    Testing out random function in JS

--- JS/homeJS.js
+++ JS/homeJS.js
@@ -1,3 +1,13 @@
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 function homeQuote() {
-    document.getElementById("Quotes").innerHTML = "Yesterday's experiences make me stronger today.";
+    const genshinQuotes =
+    [
+        "Yesterday's experiences make me stronger today.",
+        "If you lie on the grass, you can feel the heartbeat of the world.",
+        "Reality is the stillness buried deep beneath the illusion."
+    ]
+    var randomQuoteIndex = getRandomInteger(0, genshinQuotes.length)
+    document.getElementById("Quotes").innerHTML = genshinQuotes[randomQuoteIndex];
 }
